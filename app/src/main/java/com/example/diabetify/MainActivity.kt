5a0dee0d490cd1d7526f4f486d32d1c0af462332
bewe <93899302+bernarduswillson@users.noreply.github.com>
2025-02-22 20:13:29 +0700

    feat: splash screen

--- app/src/main/java/com/example/diabetify/MainActivity.kt
+++ app/src/main/java/com/example/diabetify/MainActivity.kt
@@ -3,24 +3,27 @@ package com.example.diabetify
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.diabetify.ui.theme.DiabetifyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             DiabetifyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                Box(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    contentAlignment = Alignment.Center
                 ) {
                     Greeting("Android")
                 }

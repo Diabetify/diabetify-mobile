0f84ffa5e9dd7b500b0609b9333043000356d9f2
bewe <93899302+bernarduswillson@users.noreply.github.com>
2025-02-24 19:24:26 +0700

    fix: logo size

--- app/src/main/java/com/example/diabetify/MainActivity.kt
+++ app/src/main/java/com/example/diabetify/MainActivity.kt
@@ -5,13 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.diabetify.ui.theme.DiabetifyTheme
 
@@ -21,29 +19,7 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         setContent {
             DiabetifyTheme {
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Greeting("Android")
-                }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DiabetifyTheme {
-        Greeting("Android")
     }
 }

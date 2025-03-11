9e7a3d9fe280083b5e32d335ba1de95216012c07
bewe <93899302+bernarduswillson@users.noreply.github.com>
2025-03-11 15:25:07 +0700

    feat: art

--- app/src/main/java/com/itb/diabetify/presentation/onboarding/Page.kt
+++ app/src/main/java/com/itb/diabetify/presentation/onboarding/Page.kt
@@ -16,7 +16,7 @@ val pages = listOf(
         description = "Pantau pola makanan, tidur, olahraga, dan lainnya dengan mudah. Input harian Anda membantu Diabetify memberikan wawasan personal untuk manajemen kesehatan yang lebih baik."
     ),
     Page(
-        imageResId = R.drawable.onboarding1,
+        imageResId = R.drawable.onboarding2,
         title = "Kenali Faktor\nRisiko Anda",
         description = "Analisis berbasis AI kami membantu Anda mengidentifikasi potensi faktor risiko diabetes berdasarkan gaya hidup dan data kesehatan Anda. Dapatkan wawasan yang jelas dan mulai langkah pencegahan sejak dini."
     ),
@@ -26,7 +26,7 @@ val pages = listOf(
         description = "Dapatkan rekomendasi khusus untuk mengurangi risiko diabetes dan meningkatkan kesehatan Anda. Perubahan kecil hari ini menciptakan masa depan yang lebih sehat."
     ),
     Page(
-        imageResId = R.drawable.onboarding3,
+        imageResId = R.drawable.onboarding4,
         title = "Kesehatan Anda,\nTujuan Anda",
         description = "Tetapkan tujuan kesehatan yang dipersonalisasi dan pantau progres Anda. Tetap termotivasi dan lakukan perbaikan secara bertahap dengan dukungan Diabetify."
     )

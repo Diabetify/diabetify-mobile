90bc7b57b053991c0b5e851f3437ec264a325c31
bewe <13521021@mahasiswa.itb.ac.id>
2025-05-11 17:46:43 +0700

    fix: date picker to ID language

--- app/src/main/java/com/itb/diabetify/presentation/common/DatePicker.kt
+++ app/src/main/java/com/itb/diabetify/presentation/common/DatePicker.kt
@@ -1,13 +1,27 @@
 package com.itb.diabetify.presentation.common
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DatePickerFormatter
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.itb.diabetify.R
+import com.itb.diabetify.ui.theme.poppinsFontFamily
+import java.text.SimpleDateFormat
+import java.util.Locale
 
+@SuppressLint("RememberReturnType")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DatePickerModal(
@@ -15,6 +29,33 @@ fun DatePickerModal(
     onDismiss: () -> Unit
 ) {
     val datePickerState = rememberDatePickerState()
+
+    val indonesianFormatter = remember {
+        object : DatePickerFormatter {
+            private val indonesianLocale = Locale("id", "ID")
+
+            override fun formatDate(
+                dateMillis: Long?,
+                locale: Locale,
+                forContentDescription: Boolean
+            ): String? {
+                return dateMillis?.let {
+                    val pattern = if (forContentDescription) {
+                        "EEEE, dd MMMM yyyy"
+                    } else {
+                        "d"
+                    }
+                    SimpleDateFormat(pattern, indonesianLocale).format(it)
+                }
+            }
+
+            override fun formatMonthYear(monthMillis: Long?, locale: Locale): String? {
+                return monthMillis?.let {
+                    SimpleDateFormat("MMMM yyyy", indonesianLocale).format(it)
+                }
+            }
+        }
+    }
 
     DatePickerDialog(
         onDismissRequest = onDismiss,
@@ -28,10 +69,24 @@ fun DatePickerModal(
         },
         dismissButton = {
             TextButton(onClick = onDismiss) {
-                Text("Cancel")
+                Text("Batal")
             }
         }
     ) {
-        DatePicker(state = datePickerState)
+        DatePicker(
+            state = datePickerState,
+            title = {},
+            headline = {
+                Text(
+                    text = "Pilih Tanggal",
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 25.sp,
+                    color = colorResource(id = R.color.white),
+                    modifier = Modifier.padding(20.dp)
+                )
+            },
+            dateFormatter = indonesianFormatter,
+        )
     }
 }
